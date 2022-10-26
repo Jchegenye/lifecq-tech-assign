de380e4ea71782989a8cabbaa1518c6cc80ed160
KE-MBP-B402 <ke-mbp-b402@KE-MBP-B402s-MacBook-Pro.local>
2022-10-26 21:51:15 +0300

    timeline layout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,20 +3,58 @@ import familySvg from '../assets/timeline/family.svg';
 
 function App() {
     return (
-        <div className='timeline'>
-            {/* <ul>
-    	        <li></li>
-    	        <li></li>
-    	        <li></li>
-            </ul>   */}
-            <Popover 
-                imageUrl={familySvg}
-                imageAlt={'Baby’s birth image'}
-                description={"Baby’s birth"}
-                canHover={false}
-                placement={'tc'}
-            />
-        </div>
+        <section className='timeline'>
+            
+            <div className="timeline__body">
+                <ul className="timeline__body__content">
+                    <li>
+                         <div className="timeline__body__content__start">
+                            <div className="timeline__body__start"></div>
+                            <div className="timeline__body__content__start__popover">
+                                <p className="timeline__body__content__start__popover--start">
+                                    You are here
+                                </p>
+                            </div>
+                            <div className="timeline__body__line"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="timeline__body__content__start">
+                            <div className="timeline__body__content__start__popover">
+                                <div className="timeline__body__content__circled"></div>
+                                <div className="timeline__body__content__start__popover--all">
+                                    <Popover 
+                                        imageUrl={familySvg}
+                                        imageAlt={'Baby’s birth image'}
+                                        description={"Baby’s birth"}
+                                        canHover={false}
+                                        placement={'tc'}
+                                    />
+                                </div>
+                            </div>
+                            <div className="timeline__body__line"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="timeline__body__content__start">
+                            <div className="timeline__body__content__start__popover">
+                                <div className="timeline__body__content__circled"></div>
+                                <div className="timeline__body__content__start__popover--all">
+                                    <Popover 
+                                        imageUrl={familySvg}
+                                        imageAlt={'Baby’s birth image'}
+                                        description={"Baby’s birth"}
+                                        canHover={false}
+                                        placement={'tc'}
+                                    />
+                                </div>
+                            </div>
+                            <div className="timeline__body__line"></div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
     );
 }
 
